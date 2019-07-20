fb7365bf3999d91d348641ef1a2d6f71f3cb142d
Largo <451713794@qq.com>
2019-07-20 21:18:37 +0800

    pull from Guoguirong

---
+++ src/main/java/com/chrm/mybatis/generator/plugins/CommentPlugin.java
@@ -0,0 +1,354 @@
+package com.chrm.mybatis.generator.plugins;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.*;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ *
+ *  @Author: GuiRunning 郭贵荣
+ *
+ *  @Description: 自定义注释生成
+ *
+ *  @Date: 2018/7/14 0:57
+ *
+ */
+public class CommentPlugin extends PluginAdapter {
+
+    private final Collection<CommentPlugin.Annotations> annotations;
+
+    /**
+     * CommentPlugin constructor
+     */
+    public CommentPlugin() {
+        annotations = new LinkedHashSet<Annotations>(Annotations.values().length);
+    }
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        topLevelClass.getJavaDocLines().clear();
+        topLevelClass.addJavaDocLine("/**");
+        topLevelClass.addJavaDocLine(" * Table: " + introspectedTable.getFullyQualifiedTable());
+        topLevelClass.addJavaDocLine(" * Created by Mybatis Generator @Author Largo @Date on " + date2Str(new Date()));
+        topLevelClass.addJavaDocLine(" */");
+        addAnnotations(topLevelClass);
+        return true;
+    }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        this.comment(field, introspectedTable, introspectedColumn);
+        return true;
+    }
+
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return false;
+    }
+
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return false;
+    }
+
+    private void comment(JavaElement element, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        element.getJavaDocLines().clear();
+        element.addJavaDocLine("/**");
+        String remark = introspectedColumn.getRemarks();
+        if (remark != null && remark.length() > 1) {
+            element.addJavaDocLine(" * " + remark);
+            element.addJavaDocLine(" *");
+        }
+
+        element.addJavaDocLine(" * Table:     " + introspectedTable.getFullyQualifiedTable());
+        element.addJavaDocLine(" * Column:    " + introspectedColumn.getActualColumnName());
+        element.addJavaDocLine(" * Nullable:  " + introspectedColumn.isNullable());
+        element.addJavaDocLine(" */");
+    }
+
+    @Override
+    public boolean sqlMapResultMapWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        this.commentResultMap(element, introspectedTable);
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapResultMapWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        this.commentResultMap(element, introspectedTable);
+        return true;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        super.setProperties(properties);
+
+        //@Data is default annotation
+        annotations.add(CommentPlugin.Annotations.DATA);
+
+        for (String annotationName : properties.stringPropertyNames()) {
+            if (annotationName.contains(".")) {
+                // Not an annotation name
+                continue;
+            }
+            String value = properties.getProperty(annotationName);
+            if (!Boolean.parseBoolean(value)) {
+                // The annotation is disabled, skip it
+                continue;
+            }
+            CommentPlugin.Annotations annotation = CommentPlugin.Annotations.getValueOf(annotationName);
+            if (annotation == null) {
+                continue;
+            }
+            String optionsPrefix = annotationName + ".";
+            for (String propertyName : properties.stringPropertyNames()) {
+                if (!propertyName.startsWith(optionsPrefix)) {
+                    // A property not related to this annotation
+                    continue;
+                }
+                String propertyValue = properties.getProperty(propertyName);
+                annotation.appendOptions(propertyName, propertyValue);
+                annotations.add(annotation);
+                annotations.addAll(CommentPlugin.Annotations.getDependencies(annotation));
+            }
+        }
+    }
+
+    @Override
+    public boolean clientGenerated(
+            Interface interfaze,
+            TopLevelClass topLevelClass,
+            IntrospectedTable introspectedTable
+    ) {
+        interfaze.addImportedType(new FullyQualifiedJavaType(
+                "org.apache.ibatis.annotations.Mapper"));
+        interfaze.addAnnotation("@Mapper");
+        return true;
+    }
+
+    void commentResultMap(XmlElement element, IntrospectedTable introspectedTable) {
+        List<Element> es = element.getElements();
+        if (!es.isEmpty()) {
+            String alias = introspectedTable.getTableConfiguration().getAlias();
+            int aliasLen = -1;
+            if (alias != null) {
+                aliasLen = alias.length() + 1;
+            }
+
+            Iterator<Element> it = es.iterator();
+            HashMap map = new HashMap();
+
+            while(true) {
+                while(it.hasNext()) {
+                    Element e = (Element)it.next();
+                    if (e instanceof TextElement) {
+                        it.remove();
+                    } else {
+                        XmlElement el = (XmlElement)e;
+                        List<Attribute> as = el.getAttributes();
+                        if (!as.isEmpty()) {
+                            String col = null;
+                            Iterator i$ = as.iterator();
+
+                            while(i$.hasNext()) {
+                                Attribute a = (Attribute)i$.next();
+                                if (a.getName().equalsIgnoreCase("column")) {
+                                    col = a.getValue();
+                                    break;
+                                }
+                            }
+
+                            if (col != null) {
+                                if (aliasLen > 0) {
+                                    col = col.substring(aliasLen);
+                                }
+
+                                IntrospectedColumn ic = introspectedTable.getColumn(col);
+                                if (ic != null) {
+                                    StringBuilder sb = new StringBuilder();
+                                    if (ic.getRemarks() != null && ic.getRemarks().length() > 1) {
+                                        sb.append("<!-- ");
+                                        sb.append(ic.getRemarks());
+                                        sb.append(" -->");
+                                        map.put(el, new TextElement(sb.toString()));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (map.isEmpty()) {
+                    return;
+                }
+
+                Set<Element> set = map.keySet();
+                Iterator i$ = set.iterator();
+
+                while(i$.hasNext()) {
+                    Element e = (Element)i$.next();
+                    int id = es.indexOf(e);
+                    es.add(id, (Element) map.get(e));
+                    es.add(id, new TextElement(""));
+                }
+
+                return;
+            }
+        }
+    }
+
+    @Override
+    public boolean sqlMapInsertElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        this.removeAttribute(element.getAttributes(), "parameterType");
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapInsertSelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        this.removeAttribute(element.getAttributes(), "parameterType");
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapUpdateByPrimaryKeySelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        this.removeAttribute(element.getAttributes(), "parameterType");
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapUpdateByPrimaryKeyWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        this.removeAttribute(element.getAttributes(), "parameterType");
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapUpdateByPrimaryKeyWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        this.removeAttribute(element.getAttributes(), "parameterType");
+        return true;
+    }
+
+    private void removeAttribute(List<Attribute> as, String name) {
+        if (!as.isEmpty()) {
+            Iterator it = as.iterator();
+
+            Attribute attr;
+            do {
+                if (!it.hasNext()) {
+                    return;
+                }
+
+                attr = (Attribute)it.next();
+            } while(!attr.getName().equalsIgnoreCase(name));
+
+            it.remove();
+        }
+    }
+
+    @Override
+    public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+        document.getRootElement().addElement(new TextElement(""));
+        document.getRootElement().addElement(new TextElement("<!-- ### 以上代码由MBG + CommentPlugin自动生成, 生成时间: " + (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(new Date()) + " ### -->\n\n\n"));
+        document.getRootElement().addElement(new TextElement("<!-- Your codes goes here!!! -->"));
+        document.getRootElement().addElement(new TextElement(""));
+        return true;
+    }
+
+    /**
+     * Adds the lombok annotations' imports and annotations to the class
+     *
+     * @param topLevelClass the partially implemented model class
+     */
+    private void addAnnotations(TopLevelClass topLevelClass) {
+        for (CommentPlugin.Annotations annotation : annotations) {
+            topLevelClass.addImportedType(annotation.javaType);
+            topLevelClass.addAnnotation(annotation.asAnnotation());
+        }
+    }
+
+    private enum Annotations {
+        DATA("data", "@Data", "lombok.Data"),
+        BUILDER("builder", "@Builder", "lombok.Builder"),
+        ALL_ARGS_CONSTRUCTOR("allArgsConstructor", "@AllArgsConstructor", "lombok.AllArgsConstructor"),
+        NO_ARGS_CONSTRUCTOR("noArgsConstructor", "@NoArgsConstructor", "lombok.NoArgsConstructor"),
+        ACCESSORS("accessors", "@Accessors", "lombok.experimental.Accessors"),
+        TO_STRING("toString", "@ToString", "lombok.ToString");
+
+
+        private final String paramName;
+        private final String name;
+        private final FullyQualifiedJavaType javaType;
+        private final List<String> options;
+
+
+        Annotations(String paramName, String name, String className) {
+            this.paramName = paramName;
+            this.name = name;
+            this.javaType = new FullyQualifiedJavaType(className);
+            this.options = new ArrayList<String>();
+        }
+
+        private static CommentPlugin.Annotations getValueOf(String paramName) {
+            for (CommentPlugin.Annotations annotation : CommentPlugin.Annotations.values())
+                if (String.CASE_INSENSITIVE_ORDER.compare(paramName, annotation.paramName) == 0)
+                    return annotation;
+
+            return null;
+        }
+
+        private static Collection<CommentPlugin.Annotations> getDependencies(CommentPlugin.Annotations annotation) {
+            if (annotation == ALL_ARGS_CONSTRUCTOR)
+                return Collections.singleton(NO_ARGS_CONSTRUCTOR);
+            else
+                return Collections.emptyList();
+        }
+
+        // A trivial quoting.
+        // Because Lombok annotation options type is almost String or boolean.
+        private static String quote(String value) {
+            if (Boolean.TRUE.toString().equals(value) || Boolean.FALSE.toString().equals(value))
+                // case of boolean, not passed as an array.
+                return value;
+            return value.replaceAll("[\\w]+", "\"$0\"");
+        }
+
+        private void appendOptions(String key, String value) {
+            String keyPart = key.substring(key.indexOf(".") + 1);
+            String valuePart = value.contains(",") ? String.format("{%s}", value) : value;
+            this.options.add(String.format("%s=%s", keyPart, quote(valuePart)));
+        }
+
+        private String asAnnotation() {
+            if (options.isEmpty()) {
+                return name;
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append(name);
+            sb.append("(");
+            boolean first = true;
+            for (String option : options) {
+                if (first) {
+                    first = false;
+                } else {
+                    sb.append(", ");
+                }
+                sb.append(option);
+            }
+            sb.append(")");
+            return sb.toString();
+        }
+    }
+
+    private String date2Str(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(date);
+    }
+}
